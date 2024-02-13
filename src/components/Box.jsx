fe2e4ba5481818523bc1b56ccdab9c2d2e1f1c63
rov1dev <rahmatillo722022@gmail.com>
2024-02-13 09:33:10 +0500

    main

---
+++ src/components/Box.jsx
@@ -0,0 +1,8 @@
+const Box = ({ value, onClick }) => {
+    const style = value === "X" ? "box x" : "box o";
+
+    return (
+        <button className={style} onClick={onClick}>{value}</button>
+    )
+}
+export default Box
